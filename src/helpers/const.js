dc4b56c7896b9d574752caaaeb5e5af89d59bd73
Adilet Gazybekov <gazybekov11@gmail.com>
2023-12-26 20:56:15 +0600

    save

---
+++ src/helpers/const.js
@@ -0,0 +1,2 @@
+export const API = "http://localhost:8000/products";
+export const API_CATEGORIES = "http://localhost:8000/categories";
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderHomePage from "../components/homePage/HeaderHomePage";
+
+const HomePage = () => {
+  return (
+    <div>
+      <HeaderHomePage />
+    </div>
+  );
+};
+
+export default HomePage;
